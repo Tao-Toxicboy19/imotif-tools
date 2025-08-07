d04f2c825c86fc60febc2fccad8cc209ea44e47f
thewarat <thewarat@imotif.io>
2025-08-07 22:44:45 +0700

    [FIX] imotif tools add Alias command

--- install.sh
+++ install.sh
@@ -33,3 +33,37 @@ chmod +x "$INSTALL_PATH/$BINARY_NAME"
 echo "$BINARY_NAME installed to: $INSTALL_PATH/$BINARY_NAME"
 echo ""
 echo "You can now run '$BINARY_NAME' from your terminal!"
+
+# -------------------------
+# Setup alias: itcm -> imotif-tools commit
+# -------------------------
+
+# Detect shell
+SHELL_NAME=$(basename "$SHELL")
+
+echo ""
+echo "🔗 Setting up alias: itcm → imotif-tools commit"
+
+case "$SHELL_NAME" in
+  bash)
+    SHELL_RC="$HOME/.bashrc"
+    echo "alias itcm='imotif-tools commit'" >> "$SHELL_RC"
+    echo "Alias added to $SHELL_RC"
+    ;;
+  zsh)
+    SHELL_RC="$HOME/.zshrc"
+    echo "alias itcm='imotif-tools commit'" >> "$SHELL_RC"
+    echo "Alias added to $SHELL_RC"
+    ;;
+  fish)
+    FISH_CONFIG="$HOME/.config/fish/config.fish"
+    echo "alias itcm 'imotif-tools commit'" >> "$FISH_CONFIG"
+    echo "Alias added to $FISH_CONFIG"
+    ;;
+  *)
+    echo "Unknown shell: $SHELL_NAME. Please add alias manually:"
+    echo "alias itcm='imotif-tools commit'"
+    ;;
+esac
+
+echo "Please restart your terminal or run 'source' on your shell config to use 'itcm'"
